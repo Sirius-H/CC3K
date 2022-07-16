9a00206f746afa93910737c37cc4d1ae9656ca48
y45hou <y45hou@uwaterloo.ca>
2022-07-16 16:46:32 -0400

    cc3k

--- main.cc
+++ main.cc
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    
+    ifstream inFile;
+    inFile.open("defaultFloor.txt", ios::in);
+    std::string s;
+    while (getline(inFile, s)) {
+        std::cout << s << std::endl;
+    }
+
+
 
 }
 
