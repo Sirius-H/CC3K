0d63ac6dfee22400999b514b609ffa74fb2fea7a
y45hou <y45hou@uwaterloo.ca>
2022-07-16 16:56:42 -0400

    cc3k

---
+++ coordinate.h
@@ -0,0 +1,10 @@
+#ifndef __COORDINATE_H__
+#define __COORDINATE_H__
+
+struct Coordinate {
+    int x;
+    int y;
+};
+
+
+#endif
--- grid.h
+++ grid.h
@@ -1,9 +1,26 @@
 #ifndef __GRID_H_
 #define __GRID_H_
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "coordinate.h"
+
+class Cell;
 
 class Grid {
-//Hello
-//hello again again againnnnnnn sdfskcbsibalkgbalkhcakh f
+    std::vector<std::vector<Cell*>> theGrid;
+    std::vector<std::vector<Cell*>> backupGrid;
+    Coordinate PCLocation;
+public:
+    Grid(ifstream& ifs);
+    void init(char c);
+    void updatePlayer();
+    void updateGrid();
+    bool canMoveTo(Coordinate& cdn);
+    void moveTo(Coordinate& oldCdn, Coordinate& newCdn);
+    void usePotion(Coordinate& cdn);
+    void PCAttack(Coordinate& cdn);
+
 };
 
 #endif
