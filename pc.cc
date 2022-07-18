3934c51baacafd132117258fdf330efdc8a5184f
y45hou <y45hou@uwaterloo.ca>
2022-07-18 00:06:26 -0400

    cc3k

--- pc.cc
+++ pc.cc
@@ -5,7 +5,7 @@ PC::PC(Coordinate cdn): Cell{cdn} { }
 
 int PC::getDefence() const { return Def; }
 
-int PC::attack(int NPCDefence) const { return std::ceil(((double)100 / (100 + NPCDefence)) * (Atk + potionAtkEffect)); }
+int PC::attack(int NPCDefence) const { return ceil(((double)100 / (100 + NPCDefence)) * (Atk + potionAtkEffect)); }
 
 int PC::attacked(int dmg) { // returns the actual damage to PC
     int actualDmg = dmg;
