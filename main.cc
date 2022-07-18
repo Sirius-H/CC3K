525efa2ffa5cb02d01a00e98931eb95cd1ffc97e
y45hou <y45hou@uwaterloo.ca>
2022-07-18 00:53:19 -0400

    abc

--- main.cc
+++ main.cc
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "grid.h"
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    /*
-    ifstream inFile;
-    inFile.open("defaultFloor.txt", ios::in);
-    std::string s;
-    while (getline(inFile, s)) {
-        std::cout << s << std::endl;
+    Grid* g;
+    if (argc == 3) {
+        string s = argv[2];
+        istringstream iss{s};
+        unsigned seed;
+        iss >> seed;
+        g = new Grid{argv[1], seed};
+
+    } else if (argc == 2) {
+        g = new Grid{argv[1]};
+    } else {
+        g = new Grid{"defaultFloor.txt"};
     }
-    */
-   Grid* g = new Grid{"defaultFloor.txt"};
-   delete g;
+    delete g;
 
 
 
