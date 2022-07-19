8c15539d30577a22f2e7f682b1c56a84da7a92da
Sirius <106825011+Sirius-H@users.noreply.github.com>
2022-07-19 03:03:20 +0000

    Replit-Testing

---
+++ .ccls-cache/@home@runner@CC3K/cell.h
@@ -0,0 +1,22 @@
+#ifndef __CELL_H_
+#define __CELL_H_
+#include <iostream>
+#include "coordinate.h"
+
+class Cell {
+    Coordinate cdn;
+public:
+    Cell(Coordinate cdn);
+    virtual int state() = 0;
+    virtual std::string getType() const = 0;
+    virtual std::string getName() const = 0;
+    virtual bool canStep();
+    virtual int attack(int def) const;
+    virtual int attacked(int dmg);
+    virtual int getDefence() const;
+    virtual ~Cell();
+};
+
+#endif
+
+
